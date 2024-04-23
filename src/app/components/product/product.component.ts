6fd9fa99cdae361696e171cb0f3d480825127edc
rebeccaloaustin <rebeccaloaustin@gmail.com>
2024-04-23 13:18:15 -0400

    first commit

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../../../types';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [RatingModule, FormsModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  @Input() product!: Product;
+  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+
+  ngOnInit() {
+    this.productOutput.emit(this.product);
+  }
+}
